d6fef58189b811b108872d74cbdfa4728014b66d
HatemMn <19950216+HatemMn@users.noreply.github.com>
2024-04-24 15:39:47 +0200

    feat : del typescript-eslint

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+It was created in order to address this issue : 
+
 ## Running the project :
 
 First run `npm install` to install the dependencies.
--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,11 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+// config where I do not typescript-eslint
 
-export default tseslint.config(
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
   {
-    languageOptions: {
-      parserOptions: {
-        project: [
-          "./app/mySecondApp/tsconfig.json",
-          "./app/myFirstApp/tsconfig.json",
-          "./tsconfig.eslint.json",
-        ],
-        tsconfigRootDir: import.meta.dirname,
-        projectFolderIgnoreList: ["app/myFirstApp/nestedProject/"],
-      },
-    },
-    ignores: ["app/myFirstApp/nestedProject/"],
-  }
-);
+    // If you comment the line below, you will get an (expected) linting error
+    ignores: ["app/myFirstApp/nestedProject/src/"],
+  },
+];
